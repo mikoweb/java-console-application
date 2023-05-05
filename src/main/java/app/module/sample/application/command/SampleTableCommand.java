7c411772bb43ab56ee1f8fba16d34e734c04ea26
Rafał Mikołajun <root@rmweb.pl>
2023-05-05 17:06:30 +0200

    replaced AsciiTable with Spring Shell Table

--- src/main/java/app/module/sample/application/command/SampleTableCommand.java
+++ src/main/java/app/module/sample/application/command/SampleTableCommand.java
@@ -4,11 +4,13 @@ import app.module.core.application.style.shell.DefaultShellMessageStyle;
 import app.module.core.domain.exception.FailedQueryException;
 import app.module.sample.domain.dto.SampleDTO;
 import app.module.sample.infrastructure.query.GetSampleDataFromJsonQuery;
-import de.vandermeer.asciitable.AsciiTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.table.BorderStyle;
+import org.springframework.shell.table.TableBuilder;
+import org.springframework.shell.table.TableModelBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,6 +19,7 @@ import java.util.ArrayList;
 @ShellCommandGroup("Sample")
 @Service
 final public class SampleTableCommand {
+    private static final short TABLE_MAX_WIDTH = 500;
     private final GetSampleDataFromJsonQuery getSampleDataFromJsonQuery;
 
     @Autowired
@@ -28,18 +31,18 @@ final public class SampleTableCommand {
     public void execute() {
         try {
             ArrayList<SampleDTO> sampleData = getSampleDataFromJsonQuery.getData();
-            AsciiTable table = new AsciiTable();
 
-            table.addRule();
-            table.addRow("First Name", "Last Name", "Age");
-            table.addRule();
+            TableModelBuilder<Object> modelBuilder = new TableModelBuilder<>();
+            modelBuilder.addRow().addValue("First Name").addValue("Last Name").addValue("Age");
 
             for (SampleDTO item : sampleData) {
-                table.addRow(item.fistName, item.lastName, item.age);
-                table.addRule();
+                modelBuilder.addRow().addValue(item.fistName).addValue(item.lastName).addValue(item.age);
             }
 
-            System.out.println(table.render());
+            TableBuilder tableBuilder = new TableBuilder(modelBuilder.build());
+            tableBuilder.addFullBorder(BorderStyle.oldschool);
+
+            System.out.println(tableBuilder.build().render(TABLE_MAX_WIDTH));
         } catch (FailedQueryException exception) {
             catchCannotLoadSampleData();
         }
